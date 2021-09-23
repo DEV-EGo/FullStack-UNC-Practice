e37e2d01c52fdb368bbd2279e1a4830e1cee4903
Edwin <dev.edwing@gmail.com>
2021-09-23 19:07:36 -0400

    folder structure

---
+++ JS/manifest.js
@@ -0,0 +1,74 @@
+// Instructions:
+// Create 3 variables. One called: "name", one called "pizzaCount", and one called "isHungry".
+// The name variable should be set to your name.
+// The pizzaCount variable should be set to a number.
+// The isHungry variable should be set to true or false.
+
+// FILL IN YOUR CODE HERE
+// =============================================
+
+// var totalCost = pizzaCount * 2.05;
+// var name = "Human";
+// var pizzaCount;
+// var isHungry;
+
+// alert("Hey:" + name);
+// alert("this slice cost $5 each");
+// alert("Still hungry:" + isHungry);
+// ==============================================
+// var items_toSell = [" supreme ", " bape "];
+// var bricks = "Jordan";
+// var number_of_Sales = 12;
+
+// alert("Hello: " + name);
+// alert("We have : " + items_toSell + " in stock");
+// alert("We dont recommend buying : " + bricks + " it does not sell");
+// alert(number_of_Sales + " items have been sold");
+// ==============================================
+// var places_to_vist = "New York";
+// var sports_team = "Yankees";
+// var championships = 28;
+
+// alert("welcome to the city that never sleeps " + places_to_vist);
+// alert(sports_team + " are located in the Bronx");
+// alert(
+//   championships +
+//     " championships have been brought back to the city by the " +
+//     sports_team
+// );
+// ==============================================
+// var drinks = "Russian Tea";
+// var treats = "Fruit tart";
+// var location_to_study = "Amelies";
+
+// alert("My new go to drink is the " + drinks);
+// alert(treats + " taste delicious ");
+// alert("The best place to study and hang out is " + location_to_study);
+// ==============================================
+// var newJob = "Full stack dev";
+// var oldJob = "Mechanic";
+// var theMissionIs = "Make money";
+// var legacy_to_leave = "Kids wont have to struggle like i did";
+
+// alert(" I finished the UNC " + newJob + " program ");
+
+// alert(" being a " + oldJob + " hardly pays the bills ");
+// alert(" im on a mission to " + theMissionIs + " that way " + legacy_to_leave);
+// ==============================================
+// var comfort = "Lazy";
+// var noMoney = "Broke";
+// var Ambition = "Life Style Changes";
+
+// alert(" Spent all year " + comfort);
+// alert(" This is bad it will keep you " + noMoney);
+// alert(
+//   " In order for a change stop being " + comfort + " Then your " + Ambition
+// );
+// ==============================================
+var versus = "Dipset";
+var HipHop = "Battle of old school hip hop";
+var what_day = "Thursday";
+
+alert("The LOX embarrased" + versus);
+alert(HipHop + " is aired on live stream " + what_day + "at 9pm EST");
+// ==============================================
