239199a9764f6d75d4c02e37d95d31587044edb0
Edwin <dev.edwing@gmail.com>
2021-09-26 14:04:46 -0400

    prompt /alert practice

--- JS/variables/prompt.js
+++ JS/variables/prompt.js
@@ -1,5 +1,22 @@
-var DoYouRock = confirm("The question is, do YOU rock?");
-var HowMuchDoYouRock = confirm("How much do you rock?");
+// var DoYouRock = confirm("The question is, do YOU rock?");
+// var HowMuchDoYouRock = confirm("How much do you rock?");
 // alert results
-alert(DoYouRock);
-alert(HowMuchDoYouRock);
+// alert(DoYouRock);
+// alert(HowMuchDoYouRock);
+//
+// var LetsStartGaming = confirm("Do you play video games?");
+// var InsertGame = prompt("let the games begin");
+// alert(LetsStartGaming);
+// alert(InsertGame);
+
+// var wheelstobuy = confirm("Welcome to InfiniteAuto");
+// var buytheseWheels = prompt("What Wheels are you trying to buy?");
+
+// alert(wheelstobuy);
+// alert(buytheseWheels);
+
+var deals = confirm("Are you looking for a deal?");
+var letGo = prompt("what brand?");
+
+alert(deals);
+alert(letGo);
