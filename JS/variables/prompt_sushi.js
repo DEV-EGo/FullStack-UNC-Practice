66e1bf8258240cf0021e26f456dbf02df54d4ef2
Edwin <dev.edwing@gmail.com>
2021-09-27 12:57:08 -0400

    prompt challenge

---
+++ JS/variables/prompt_sushi.js
@@ -0,0 +1,12 @@
+// ### Instructions
+
+// * Write JavaScript code that does the following:
+//   * Using a confirm, ask the user, "Do you like \_?" Store their response in a variable.
+//   * Using a prompt, ask the user, "What kind of \_ do you like?" Store their response in a variable.
+//   * Alert both variables to the screen.
+
+var DoYoulike = confirm("Do you like Marvel?");
+var WhatKindOfDoYoulike = prompt("What Marvel character do you like?");
+
+alert("You like:" + DoYoulike);
+alert("Tester likes?" + WhatKindOfDoYoulike + " lets fight spiderman");
