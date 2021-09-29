94de80faaba2c91a9d572b4abdf5e164d1b4f010
Edwin <dev.edwing@gmail.com>
2021-09-28 21:16:44 -0400

    acess if else

--- JS/variables/conditional.js
+++ JS/variables/conditional.js
@@ -18,19 +18,32 @@
 
 // =================================================================================================
 // Here we create our prompt and confirm variables which will store user input.
-var userAcess_input = confirm("Do you want acess?");
-var userConfirm_acess = confirm("Are you sure youre ready to enter?");
-var user_Identifier = prompt("What are you trying to learn about?");
+// var userAcess_input = confirm("Do you want acess?");
+// var userConfirm_acess = confirm("Are you sure youre ready to enter?");
+// var user_Identifier = prompt("What are you trying to learn about?");
 
 // If the user likes sushi (confirmSushi === true), we run the following block of code.
-if (userConfirm_acess) {
-  alert(" test " + userAcess_input);
-}
+// if (userConfirm_acess) {
+//   alert(" test " + userAcess_input);
+// }
 // If the user likes ginger tea (confirmGingerTea === true), we run the following block of code.
-else if (userConfirm_acess) {
-  alert(" test 2");
-}
+// else if (userConfirm_acess) {
+//   alert(" test 2");
+// }
 // If neither of the previous condition were true, we run the following block of code.
-else {
-  document.write("hmmmm denied");
+// else {
+//   document.write("hmmmm denied");
+// }
+// =================================================================================================
+
+var snkrs_access = confirm("");
+var snkrs_confirm = confirm("");
+var snkrs_take_thisL = prompt("");
+
+if (snkrs_access) {
+  alert("test" + snkrs_confirm);
+} else if (snkrs_confirm) {
+  aler("");
+} else {
+  document.write("mm...");
 }
