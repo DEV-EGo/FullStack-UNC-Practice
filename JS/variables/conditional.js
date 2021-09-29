59549ba26667ac943376a6440261604ca2dfc639
Edwin <dev.edwing@gmail.com>
2021-09-28 21:05:19 -0400

    if else

--- JS/variables/conditional.js
+++ JS/variables/conditional.js
@@ -1,26 +1,36 @@
 // Here we create our prompt and confirm variables which will store user input.
-var confirmSushi = confirm("Do you like sushi?");
-var confirmGingerTea = confirm("Do you like Ginger Tea?");
-var sushiType = prompt("What kind of sushi do you like?");
+// var confirmSushi = confirm("Do you like sushi?");
+// var confirmGingerTea = confirm("Do you like Ginger Tea?");
+// var sushiType = prompt("What kind of sushi do you like?");
 
 // If the user likes sushi (confirmSushi === true), we run the following block of code.
-if (confirmSushi) {
-  alert("You like " + sushiType + "!");
-}
+// if (confirmSushi) {
+//   alert("You like " + sushiType + "!");
+// }
 // If the user likes ginger tea (confirmGingerTea === true), we run the following block of code.
-else if (confirmGingerTea) {
-  alert("You like ginger tea!!");
-}
+// else if (confirmGingerTea) {
+//   alert("You like ginger tea!!");
+// }
 // If neither of the previous condition were true, we run the following block of code.
-else {
-  alert("You don't like sushi or ginger tea.");
-}
+// else {
+//   alert("You don't like sushi or ginger tea.");
+// }
 
 // =================================================================================================
 // Here we create our prompt and confirm variables which will store user input.
+var userAcess_input = confirm("Do you want acess?");
+var userConfirm_acess = confirm("Are you sure youre ready to enter?");
+var user_Identifier = prompt("What are you trying to learn about?");
 
 // If the user likes sushi (confirmSushi === true), we run the following block of code.
-
+if (userConfirm_acess) {
+  alert(" test " + userAcess_input);
+}
 // If the user likes ginger tea (confirmGingerTea === true), we run the following block of code.
-
+else if (userConfirm_acess) {
+  alert(" test 2");
+}
 // If neither of the previous condition were true, we run the following block of code.
+else {
+  document.write("hmmmm denied");
+}
