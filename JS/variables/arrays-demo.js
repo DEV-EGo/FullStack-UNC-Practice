3023bb2be1c89e27dae39b4f6d99afa8594375c0
Edwin <dev.edwing@gmail.com>
2021-10-02 18:47:50 -0400

    Array console log practice

---
+++ JS/variables/arrays-demo.js
@@ -0,0 +1,36 @@
+// Our array of zoo animals.
+var zooAnimals = ["Zebra", "Rhino", "Giraffe", "Owl"];
+// Prints 4 to the console because there are 4 items in our zooAnimals array.
+console.log(zooAnimals.length);
+// Prints Rhino to the console. Remember, the first item in an array has an index position of 0!
+console.log(zooAnimals[1]);
+// Prints undefined... because the last index ("Owl") is 3.
+console.log(zooAnimals[4]);
+// =================================================================
+var videoGames = ["SpiderMan", "GTA", "COD", "Fortnite", "Crash", "God Of War"];
+console.log(videoGames.length);
+console.log(videoGames[3]);
+console.log(videoGames[5]);
+// =================================================================
+var SneakersToBuy = [
+  "Offwhite",
+  "NIKE Yeezy",
+  "Air Mags",
+  "Cactus Jack 4's",
+  "eminem 4's",
+];
+console.log(SneakersToBuy.length);
+console.log(SneakersToBuy[2]);
+console.log(SneakersToBuy[5]);
+// =================================================================
+var Gaming_Consoles = [
+  "PS4",
+  "XBOX",
+  "Nintendo Switch",
+  "PSP",
+  "Gameboy",
+  "Sega Dreamcast",
+];
+console.log(Gaming_Consoles.length);
+console.log(Gaming_Consoles[4]);
+console.log(Gaming_Consoles[7]);
