a0c221d53d0e7c4a93619f26fbc1ffd9d1f30227
Edwin <dev.edwing@gmail.com>
2021-10-08 12:53:00 -0400

    array solution

--- JS/variables/ppl_array.js
+++ JS/variables/ppl_array.js
@@ -18,5 +18,74 @@ console.log(coolPeople[2]);
 console.log(coolPeople[3]);
 console.log(coolPeople[4]);
 console.log(coolPeople[5]);
+console.log("================================================");
 
 // ...
+var foodtoeat = [
+  "Bojangles",
+  "McDondalds",
+  "BurgerKing",
+  "Taco Bell",
+  "Chick Fil A",
+  "In N Out",
+  "Zaxbys",
+];
+
+console.log(foodtoeat[0]);
+console.log(foodtoeat[1]);
+console.log(foodtoeat[2]);
+console.log(foodtoeat[3]);
+console.log(foodtoeat[4]);
+console.log(foodtoeat[5]);
+console.log(foodtoeat[6]);
+
+console.log("================================================");
+var drinks = [
+  "Pepsi",
+  "CocaCola",
+  "Dr. Pepper",
+  "Sprite",
+  "Sweat Tea",
+  "Fanta",
+];
+
+console.log(drinks[0]);
+console.log(drinks[1]);
+console.log(drinks[2]);
+console.log(drinks[3]);
+console.log(drinks[4]);
+console.log(drinks[5]);
+
+console.log("================================================");
+
+var placestovisit = [
+  "Greece",
+  "Roma",
+  "Amsterdam",
+  "Madrid",
+  "Brazil",
+  "Japan",
+];
+
+console.log(placestovisit[0]);
+console.log(placestovisit[1]);
+console.log(placestovisit[2]);
+console.log(placestovisit[3]);
+console.log(placestovisit[4]);
+console.log(placestovisit[5]);
+
+console.log("================================================");
+
+var sneakerToBuy = [
+  "Jordans",
+  "Foamposites",
+  "Timberland",
+  "OFf_White",
+  "BapeStas",
+];
+
+console.log(sneakerToBuy[0]);
+console.log(sneakerToBuy[1]);
+console.log(sneakerToBuy[2]);
+console.log(sneakerToBuy[3]);
+console.log(sneakerToBuy[4]);
