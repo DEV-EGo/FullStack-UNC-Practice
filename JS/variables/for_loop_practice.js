84f2a8e684745a0609589e7fc17dd01c6299cd88
Edwin <dev.edwing@gmail.com>
2021-10-12 12:46:18 -0400

    for loop

---
+++ JS/variables/for_loop_practice.js
@@ -0,0 +1,47 @@
+var veggies = [
+  "Carrots",
+  "Peas",
+  "Lettuce",
+  "Tomatoes",
+  "Lettuce",
+  "Brocolli",
+  "Potatoe",
+];
+
+for (var i = 0; i < veggies.length; i++) {
+  console.log("I love " + veggies[i]);
+}
+
+console.log("================================================================");
+
+var videoGames = [
+  "Metroid",
+  "DOOM",
+  "Mario 64",
+  "FIFA",
+  "Monkey Ball",
+  "DBZ",
+  "Pokemon",
+];
+
+for (var i = 0; i < videoGames.length; i++) {
+  console.log("I love " + videoGames[i]);
+}
+console.log("================================================================");
+
+var JDM_Bro = [
+  "Honda NSX",
+  "Acura NSX",
+  "GTR",
+  "S2000",
+  "GT86",
+  "Supra",
+  "McClaren",
+  "Ferrari",
+  "Hell Cat",
+];
+
+for (var i = 0; i < JDM_Bro.length; i++) {
+  console.log(" Lets Race " + JDM_Bro[i]);
+}
+console.log("================================================================");
