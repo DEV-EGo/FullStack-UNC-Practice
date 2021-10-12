8d90ffc137275b72d186ea2037aaa3512122799a
Edwin <dev.edwing@gmail.com>
2021-10-12 12:29:39 -0400

    add .

--- JS/variables/array-setting.js
+++ JS/variables/array-setting.js
@@ -22,10 +22,10 @@ console.log(drinks[1]);
 console.log(drinks[2]);
 console.log(drinks[3]);
 // We can either overwrite each element of the array to make it lowercase, or we can use the toLowerCase() method.
-drinks[0] = "coke";
-drinks[1] = "pepsi";
-drinks[2] = "water";
-drinks[3] = drinks[3].toLowerCase();
+// drinks[0] = "coke";
+// drinks[1] = "pepsi";
+// drinks[2] = "water";
+// drinks[3] = drinks[3].toLowerCase();
 
 console.log("==============================================================");
 
@@ -37,3 +37,40 @@ console.log(drinks[2]);
 console.log(drinks[3]);
 
 console.log("==============================================================");
+
+var soup = [
+  "Chicken Noodle",
+  "Brocolli",
+  "Noodles",
+  "Letter Soup",
+  "Cheddar",
+  "Beef Soup",
+];
+
+console.log("1st practice run");
+console.log(soup[0]);
+console.log(soup[1]);
+console.log(soup[2]);
+console.log(soup[3]);
+console.log(soup[4]);
+console.log(soup[5]);
+
+// We can either overwrite each element of the array to make it lowercase, or we can use the toLowerCase() method.
+soup[0] = "Chicken Noodle";
+soup[1] = "Brocolli";
+soup[2] = "Noodle";
+soup[3] = "Noodles";
+soup[4] = "Letter Soup";
+soup[5] = "Cheddar";
+soup[6] = "Beef Soup".toLowerCase();
+
+console.log("==============================================================");
+
+console.log("second practice");
+console.log(soup[0]);
+console.log(soup[1]);
+console.log(soup[2]);
+console.log(soup[3]);
+console.log(soup[4]);
+console.log(soup[5]);
+console.log(soup[6]);
