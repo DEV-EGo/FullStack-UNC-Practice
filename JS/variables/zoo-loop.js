4725cd8e52f072e25ba79cab23760a0c42f90c16
Edwin <dev.edwing@gmail.com>
2021-10-14 12:36:25 -0400

    comments out

--- JS/variables/zoo-loop.js
+++ JS/variables/zoo-loop.js
@@ -8,150 +8,150 @@ var zooAnimals = ["Zebra", "Rhino", "Giraffe", "Owl"];
 // console.log(zooAnimals[3]);
 
 // for loop
-for (var i = 0; i < zooAnimals.length; i++) {
-  console.log(zooAnimals[i]);
-}
+// for (var i = 0; i < zooAnimals.length; i++) {
+//   console.log(zooAnimals[i]);
+// }
 
 console.log("================================================");
 
 // calling function
-function logArray(list) {
-  for (var j = 0; j < zooAnimals.length; j++) {
-    console.log(zooAnimals[j]);
-  }
-}
-logArray(zooAnimals);
+// function logArray(list) {
+//   for (var j = 0; j < zooAnimals.length; j++) {
+//     console.log(zooAnimals[j]);
+//   }
+// }
+// logArray(zooAnimals);
 
 console.log("================================================");
 
-var sodas = [
-  "Pepsi",
-  "Coca-Cola",
-  "Sprite",
-  "Dr.Pepper",
-  "Orange Fanta",
-  "Mountain Dew",
-  "Monster",
-];
+// var sodas = [
+//   "Pepsi",
+//   "Coca-Cola",
+//   "Sprite",
+//   "Dr.Pepper",
+//   "Orange Fanta",
+//   "Mountain Dew",
+//   "Monster",
+// ];
 
-for (var s = 0; s < sodas.length; s++) {
-  console.log(sodas[s]);
-}
+// for (var s = 0; s < sodas.length; s++) {
+//   console.log(sodas[s]);
+// }
 console.log("================function============================");
 
-function sodaList(thislist) {
-  for (var t = 0; t < thislist.length; t++) {
-    console.log(thislist[t]);
-  }
-}
-sodaList(sodas);
+// function sodaList(thislist) {
+//   for (var t = 0; t < thislist.length; t++) {
+//     console.log(thislist[t]);
+//   }
+// }
+// sodaList(sodas);
 
 console.log("================================================");
 
-var artist = [
-  "jay-z",
-  "Nas",
-  "2pac",
-  "Biggie",
-  "Big L",
-  "Big Pun",
-  "Wu-Tang",
-  "Juice Wrld",
-];
+// var artist = [
+//   "jay-z",
+//   "Nas",
+//   "2pac",
+//   "Biggie",
+//   "Big L",
+//   "Big Pun",
+//   "Wu-Tang",
+//   "Juice Wrld",
+// ];
 
-for (var n = 0; n < artist.length; n++) {
-  console.log(artist[n]);
-}
+// for (var n = 0; n < artist.length; n++) {
+//   console.log(artist[n]);
+// }
 console.log("================function============================");
-function rapper(artistList) {
-  for (var u = 0; u < artistList.length; u++) {
-    console.log(artistList[u]);
-  }
-}
-rapper(artist);
+// function rapper(artistList) {
+//   for (var u = 0; u < artistList.length; u++) {
+//     console.log(artistList[u]);
+//   }
+// }
+// rapper(artist);
 console.log("================================================");
 
-var rockband = [
-  "LINKIN PARK",
-  "The Rollingstones",
-  "Nirvana",
-  "The Doors",
-  "Rammnstein",
-  "3 days grace",
-  "Shinedown",
-];
+// var rockband = [
+//   "LINKIN PARK",
+//   "The Rollingstones",
+//   "Nirvana",
+//   "The Doors",
+//   "Rammnstein",
+//   "3 days grace",
+//   "Shinedown",
+// ];
 
-for (var s = 0; s < rockband.length; s++) {
-  console.log(rockband[s]);
-}
+// for (var s = 0; s < rockband.length; s++) {
+//   console.log(rockband[s]);
+// }
 console.log("================function============================");
-function letsRock(rockbands) {
-  for (var r = 0; r < rockbands.length; r++) {
-    console.log(rockbands[r]);
-  }
-}
-letsRock(rockband);
+// function letsRock(rockbands) {
+//   for (var r = 0; r < rockbands.length; r++) {
+//     console.log(rockbands[r]);
+//   }
+// }
+// letsRock(rockband);
 
 console.log("============================================");
 
-var cellphone = [
-  "Iphone",
-  "SideKick",
-  "Boost",
-  "Nextel",
-  "Samsung",
-  "Galaxys10",
-];
+// var cellphone = [
+//   "Iphone",
+//   "SideKick",
+//   "Boost",
+//   "Nextel",
+//   "Samsung",
+//   "Galaxys10",
+// ];
 
-for (var k = 0; k < cellphone.length; k++) {
-  console.log(cellphone[k]);
-}
+// for (var k = 0; k < cellphone.length; k++) {
+//   console.log(cellphone[k]);
+// }
 
 console.log("================function============================");
-function cellular(PhonesToSell) {
-  for (var l = 0; l < PhonesToSell.length; l++) {
-    console.log(PhonesToSell[l]);
-  }
-}
-cellular(cellphone);
+// function cellular(PhonesToSell) {
+//   for (var l = 0; l < PhonesToSell.length; l++) {
+//     console.log(PhonesToSell[l]);
+//   }
+// }
+// cellular(cellphone);
 console.log("============================================");
 
-var mainCharacters = [
-  "Mario",
-  "DOOM Guy",
-  "Metroid",
-  "Donkey Kong",
-  "Venom",
-  "carnage",
-  "spiderman",
-];
+// var mainCharacters = [
+//   "Mario",
+//   "DOOM Guy",
+//   "Metroid",
+//   "Donkey Kong",
+//   "Venom",
+//   "carnage",
+//   "spiderman",
+// ];
 
-for (var w = 0; w < mainCharacters.length; w++) {
-  console.log(mainCharacters[w]);
-}
+// for (var w = 0; w < mainCharacters.length; w++) {
+//   console.log(mainCharacters[w]);
+// }
 console.log("================function============================");
-function ChooseAcharacter(MainPlayer) {
-  for (var e = 0; e < MainPlayer.length; e++) {
-    console.log(MainPlayer[e]);
-  }
-}
-ChooseAcharacter(mainCharacters);
+// function ChooseAcharacter(MainPlayer) {
+//   for (var e = 0; e < MainPlayer.length; e++) {
+//     console.log(MainPlayer[e]);
+//   }
+// }
+// ChooseAcharacter(mainCharacters);
 console.log("================function========================");
-var HIPHOP = [
-  "A tribe called quest",
-  "Wu-Tang",
-  "Terror Squad",
-  "Jr Mafia",
-  "Mobb Deep",
-  "NWA",
-];
+// var HIPHOP = [
+//   "A tribe called quest",
+//   "Wu-Tang",
+//   "Terror Squad",
+//   "Jr Mafia",
+//   "Mobb Deep",
+//   "NWA",
+// ];
 
-function oldSchoolHipHop(RealRap) {
-  for (var q = 0; q < RealRap.length; q++) {
-    console.log(RealRap[q]);
-  }
-}
-oldSchoolHipHop(HIPHOP);
+// function oldSchoolHipHop(RealRap) {
+//   for (var q = 0; q < RealRap.length; q++) {
+//     console.log(RealRap[q]);
+//   }
+// }
+// oldSchoolHipHop(HIPHOP);
 
 console.log("============================================");
 
@@ -160,9 +160,9 @@ console.log("============================================");
 //   console.log(" i am " + i_am[y]);
 // }
 
-for (var b = 0; b < 5; b++) {
-  console.log(" i am " + b);
-}
+// for (var b = 0; b < 5; b++) {
+//   console.log(" i am " + b);
+// }
 
 console.log("============================================");
 
@@ -185,3 +185,5 @@ for (var h = 0; h < listof_wheels_to_buy; h++) {
     alert("starts with a F or an O!");
   }
 }
+
+console.log("============================================");
