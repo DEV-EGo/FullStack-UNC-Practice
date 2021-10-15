af68555b7b5df619d5b054b3d0f5fbaf4a8c8e88
Edwin <dev.edwing@gmail.com>
2021-10-15 12:36:40 -0400

    user text 2

--- JS/variables/events-example.js
+++ JS/variables/events-example.js
@@ -2,3 +2,8 @@ var usertext = document.getElementById("user-text");
 document.onkeyup = function (event) {
   usertext.textContent = event.key;
 };
+
+var user_text_2 = document.getElementById("user-text-2");
+document.onkeyup = function (event) {
+  user_text_2.textContent = event.key;
+};
