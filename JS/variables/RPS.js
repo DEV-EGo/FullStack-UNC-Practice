3b37f75d75cfd1e3300c65c2e7272f2a21ef522f
Edwin <dev.edwing@gmail.com>
2021-10-15 21:57:29 -0400

    more practice with event handles

--- JS/variables/RPS.js
+++ JS/variables/RPS.js
@@ -1,21 +1,32 @@
 // creating back end for rock-paper-scissor game
 
 // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
-var computer_choices = ["r", "p", "s"];
+// var computer_choices = ["r", "p", "s"];
 
 // function below runs whenever the user presses a key.
+// document.onkeyup = function (event) {
+// determines what key was pressed
+//   var GuessByUser = event.key;
+
+// Choses randomly from the options array (computers guess)
+
+//   var GuessByComputer =
+// computer_choices[Math.floor(Math.random() * computer_choices.length)];
+
+// alerts key that user pressed
+//   alert(" Player one chose " + GuessByUser);
+
+//   alert the computer guess
+//   alert(" Computer chose " + GuessByComputer);
+// };
+// =================================================================
+
+var youpick = ["y", "u", "i"];
+
 document.onkeyup = function (event) {
-  // determines what key was pressed
-  var GuessByUser = event.key;
+  var uGuess = event.key;
+  var cGuess = youpick[Math.floor(Math.random() * youpick.length)];
 
-  // Choses randomly from the options array (computers guess)
-
-  var GuessByComputer =
-    computer_choices[Math.floor(Math.random() * computer_choices.length)];
-
-  // alerts key that user pressed
-  alert(" Player one chose " + GuessByUser);
-
-  //   alert the computer guess
-  alert(" Computer chose " + GuessByComputer);
+  alert(" you guessed:" + uGuess);
+  alert(" AI chose:" + cGuess);
 };
