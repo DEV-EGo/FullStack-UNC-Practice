26166592cfed361f86ca73789cbed8b1e897b2bf
Edwin <dev.edwing@gmail.com>
2021-10-19 12:47:07 -0400

    working solution

--- JS/variables/scripts/rps_2.js
+++ JS/variables/scripts/rps_2.js
@@ -11,3 +11,38 @@ var computerChoiceText = document.getElementById("computerchoice-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var tiesText = document.getElementById("ties-text");
+
+// function will fire when user hits keyboard
+document.onkeyup = function (event) {
+  // determines what key was hit
+  var user_Guess = event.key;
+
+  // randomly chooses a choice from the option array
+  // this is the computer guess
+  var computer_Guess =
+    choices_computer[Math.floor(Math.random() * choices_computer.length)];
+
+  // "else if" will determine the outcome of the game (win/loss/tie)
+  if((user_Guess === "r") && || (user_Guess === "p") ||(user_Guess === "s")){
+
+    if((user_Guess === "r"&&computer_Guess === "s")||
+    (user_Guess === "s" && computer_Guess === "p") ||
+    (user_Guess === "p" && computer_Guess === "r")){
+        wins++;
+    }else if(user_Guess === computer_Guess){
+        ties++;
+    }else{
+        losses++;
+    }
+
+    // hide the direction
+    directionsText.textContent = "";
+    
+    // Display the user and computer guesses, wins/losses/ties
+    userChoiceText.textContent = "you chose:" + user_Guess;
+    computerChoiceText.textContent = "the computer chose:" + computer_Guess;
+    winsText.textContent = "wins:" + wins;
+    lossesText.textContent = "losss:" + losses;
+    tiesText.textContent = "ties" + ties;
+  }
+};
