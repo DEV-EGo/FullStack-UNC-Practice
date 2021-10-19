bf1d67f380a51e251706f8a64e52d7906b0c4ba4
Edwin <dev.edwing@gmail.com>
2021-10-19 12:23:31 -0400

    targetint html

---
+++ JS/variables/scripts/rps_2.js
@@ -0,0 +1,13 @@
+var choices_computer = ["r", "p", "s"];
+// variables that will hold scores
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+// variables that will target references in the html
+var directionsText = document.getElementById("direction-text");
+var userChoiceText = document.getElementById("userchoice-text");
+var computerChoiceText = document.getElementById("computerchoice-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var tiesText = document.getElementById("ties-text");
