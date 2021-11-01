ccf3432329849f5a0e5bf11c799a37c0c5aba9fe
Edwin <dev.edwing@gmail.com>
2021-11-01 12:50:06 -0400

    loop.js

---
+++ JS/variables/scripts/loop-tv.js
@@ -0,0 +1,28 @@
+// answer the following
+
+// ????
+// var favTVshows = ["Sons of anarchy", "YOU", "Squid Games", "Breaking Bad"];
+// var tvShow = ["DBZ", "naruto", "death note", "attack on titan"];
+
+// For Loop Elements Repeats ??? Times
+// for (var i = 1; i < 4; i++) {
+//   console.log(favTVshows[i]);
+// ????
+// tvShow = prompt("What's your #" + i + " favorite TV show?");
+
+// ????
+// favTVshows.push(tvShow);
+// }
+
+// ????
+// for (var j = 0; j < favTVshows.length; j++) {
+// ???
+//   alert(favTVshows[j]);
+// }
+
+var problems = ["Fights", "Arguments", "Cold Shoulder"];
+var solutions = ["therapy", "prayer", "hobbies"];
+
+for (var w = 0; w < problems.length; w++) {
+  console.log(problems[w]);
+}
