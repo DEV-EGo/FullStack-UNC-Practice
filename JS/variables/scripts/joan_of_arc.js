5314d4ddf84746c3d6e7db85ab293bd12e22bbf2
Edwin <dev.edwing@gmail.com>
2021-11-12 12:47:46 -0500

    joan of arc

---
+++ JS/variables/scripts/joan_of_arc.js
@@ -0,0 +1,22 @@
+// properties
+var joanOfArcInfo = [
+  "Real Name",
+  "Grew Up Where",
+  "Known For",
+  "Scars",
+  "Symbolism",
+];
+
+// values for the properties
+var joanOfArcValues = [
+  "Jehanne la Pucelle.",
+  "Domremy, a village in northeastern France.",
+  "Peasant girl, daughter of a farmer, who rose to become Commander of the French army.",
+  "Took an arrow to the shoulder and a crossbow bolt to the thigh while trying to liberate Paris.",
+  "Stands for French unity and nationalism.",
+];
+
+var Input = "Real Name";
+
+// how to use input to retrieve data
+var index = joanOfArcInfo.indexOf(Input);
