7b24f396a0bd49b57b4acecb0dc050eb6a64455b
Edwin <dev.edwing@gmail.com>
2022-01-03 20:22:10 -0500

    RPS working

--- JS/variables/scripts/rps_2.js
+++ JS/variables/scripts/rps_2.js
@@ -1,85 +1,51 @@
 var choices_computer = ["r", "p", "s"];
 // variables that will hold scores
-var Wins = 0;
+var wins = 0;
 var loss = 0;
-var Ties = 0;
+var ties = 0;
 
 // variables that will target references in the html
-// var directionsText = document.getElementById("direction-text");
-// var userChoiceText = document.getElementById("userchoice-text");
-// var computerChoiceText = document.getElementById("computerchoice-text");
-// var winsText = document.getElementById("wins-text");
-// var lossesText = document.getElementById("losses-text");
-// var tiesText = document.getElementById("ties-text");
+var directionsText = document.getElementById("direction-text");
+var userChoiceText = document.getElementById("userchoice-text");
+var computerChoiceText = document.getElementById("computerchoice-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var tiesText = document.getElementById("ties-text");
 
 // ===============================
 
-var textDirections = document.getElementById("directions-text");
-var ChoicetextComputer = document.getElementById("userchoice-text");
-var TextUserChoice = document.getElementById("computerchoice-text");
-var textWins = document.getElementById("wins-text");
-var textlosses = document.getElementById("losses-text");
-var textTies = document.getElementById("ties-text");
-
+// function will fire when user hits keyboard
 document.onkeyup = function (event) {
-  var Guess_byUser = event.key;
+  // determines what key was hit
+  var user_Guess = event.key;
 
-  var Guess_Computing =
-    choices_computer[Math.floor(Math.random() * Guess_Computing.length)];
-  if (Guess_byUser === "r" || Guess_byUser === "p" || Guess_byUser === "s") {
+  // randomly chooses a choice from the option array
+  // this is the computer guess
+  var computer_Guess =
+    choices_computer[Math.floor(Math.random() * choices_computer.length)];
+
+  // "else if" will determine the outcome of the game (win/loss/tie)
+  if (user_Guess === "r" || user_Guess === "p" || user_Guess === "s") {
     if (
-      (Guess_byUser === "r" && Guess_byUser === "s") ||
-      (Guess_byUser === "s" && Guess_byUser === "p") ||
-      (Guess_byUser === "p" && Guess_byUser === "r")
+      (user_Guess === "r" && computer_Guess === "s") ||
+      (user_Guess === "s" && computer_Guess === "p") ||
+      (user_Guess === "p" && computer_Guess === "r")
     ) {
-      Wins++;
-    } else if (Guess_byUser === Guess_Computing) {
-      Ties++;
+      wins++;
+    } else if (user_Guess === computer_Guess) {
+      ties++;
     } else {
       loss++;
     }
 
+    // hide the direction
     directionsText.textContent = "";
 
-    textDirections.textContent = "you chose:" + Guess_byUser;
-    ChoicetextComputer.textContent = "the computer chose:" + Guess_byUser;
-    textWins.textContent = "wins:" + Wins;
-    textlosses.textContent = "losses:" + loss;
-    textTies.textContent = "ties" + Ties;
+    // Display the user and computer guesses, wins/losses/ties
+    userChoiceText.textContent = "you chose:" + user_Guess;
+    computerChoiceText.textContent = "the computer chose:" + computer_Guess;
+    winsText.textContent = "wins:" + wins;
+    lossesText.textContent = "losss:" + loss;
+    tiesText.textContent = "ties" + ties;
   }
 };
-// function will fire when user hits keyboard
-// document.onkeyup = function (event) {
-// determines what key was hit
-// var user_Guess = event.key;
-
-// randomly chooses a choice from the option array
-// this is the computer guess
-// var computer_Guess =
-//   choices_computer[Math.floor(Math.random() * choices_computer.length)];
-
-// "else if" will determine the outcome of the game (win/loss/tie)
-// if (user_Guess === "r" || user_Guess === "p" || user_Guess === "s") {
-//   if (
-//     (user_Guess === "r" && computer_Guess === "s") ||
-//     (user_Guess === "s" && computer_Guess === "p") ||
-//     (user_Guess === "p" && computer_Guess === "r")
-//   ) {
-//     wins++;
-//   } else if (user_Guess === computer_Guess) {
-//     ties++;
-//   } else {
-//     losses++;
-//   }
-
-// hide the direction
-// directionsText.textContent = "";
-
-// Display the user and computer guesses, wins/losses/ties
-//     userChoiceText.textContent = "you chose:" + user_Guess;
-//     computerChoiceText.textContent = "the computer chose:" + computer_Guess;
-//     winsText.textContent = "wins:" + wins;
-//     lossesText.textContent = "losss:" + losses;
-//     tiesText.textContent = "ties" + ties;
-//   }
-// };
