a265bf3a993f345bb9f0d9e9d6165c0bfa4dddb2
Edwin <dev.edwing@gmail.com>
2022-01-04 12:42:34 -0500

    for loop practice

---
+++ JS/variables/scripts/superhero_logging.js
@@ -0,0 +1,76 @@
+// Various Arrays
+var brands = ["Acer", "Apple", "Sony", "Samsung"];
+var heroes = [
+  "Black Panther",
+  "Cyborg",
+  "Black Canary",
+  "Donna Troy",
+  "Huntress",
+  "Blue Beetle",
+  "Captain Atom",
+];
+var booksOnMyShelf = [
+  "Calculus Early Transcendentals",
+  "Ravens",
+  "The Self Illusion",
+  "Harry Potter",
+];
+var thingsInFrontOfMe = ["Laptop", "Beanbag", "Cats", "Slippers"];
+var howIFeel = ["Sleep Deprived", "Wired on Coffee", "Excited"];
+
+// For Loop for Brands
+for (var i = 0; i < brands.length; i++) {
+  console.log(brands[i]);
+}
+console.log("---------");
+
+// For Loop for Heroes
+for (var j = 0; j < heroes.length; j++) {
+  console.log(heroes[j]);
+}
+console.log("---------");
+
+// For Loop for booksOnMyShelf
+for (var k = 0; k < booksOnMyShelf.length; k++) {
+  console.log(booksOnMyShelf[k]);
+}
+console.log("---------");
+
+// For Loop for thingsInFrontOfMe
+for (var l = 0; l < thingsInFrontOfMe.length; l++) {
+  console.log(thingsInFrontOfMe[l]);
+}
+console.log("---------");
+
+// For Loop for howIFeel
+for (var m = 0; m < howIFeel.length; m++) {
+  console.log(howIFeel[m]);
+}
+console.log("============More practice==================");
+
+var soda = ["pepsi", "coca cola", "sprite", "fanta"];
+var food = ["popcorn", "hot dog", "nachos", "eminems"];
+var movies = ["The matrix", "Dont look up", "encanto", "Spider-Man"];
+var theaters = ["IMAX", "AMC", "DOLBY", "Digital"];
+
+// for loop sodas
+for (var s = 0; s < food.length; s++) {
+  console.log(soda[s]);
+}
+console.log("---------");
+// for loop food
+for (var f = 0; f < food.length; f++) {
+  console.log(food[f]);
+}
+console.log("---------");
+// for loop movies
+for (var m = 0; m < movies.length; m++) {
+  console.log(movies[m]);
+}
+console.log("---------");
+
+// for loop theaters
+for (var t = 0; t < theaters.length; t++) {
+  console.log(theaters[t]);
+}
+console.log("============More practice==================");
