388e43478fa9dc3e70031db6755ed4910276c328
Edwin <dev.edwing@gmail.com>
2022-01-05 18:06:35 -0500

    objects

---
+++ JS/variables/scripts/gandalf.js
@@ -0,0 +1,13 @@
+var gandalf = {
+  "real name": "Gandalf",
+  "age (est)": 11000,
+  race: "Maia",
+  haveRetirementPlan: true,
+  aliases: [
+    "Greyhame",
+    "Stormcrow",
+    "Mithrandir",
+    "Gandalf the Grey",
+    "Gandalf the White",
+  ],
+};
