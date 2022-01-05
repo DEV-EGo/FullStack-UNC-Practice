b9c02ce577bdf0f34e29c4fdf1ec956dcf79693a
Edwin <dev.edwing@gmail.com>
2022-01-05 18:30:45 -0500

    props practice

--- JS/variables/scripts/gandalf.js
+++ JS/variables/scripts/gandalf.js
@@ -11,3 +11,21 @@ var gandalf = {
     "Gandalf the White",
   ],
 };
+// accesing properties with "bracket notation"
+// alert("my name is " + gandalf["real name"]);
+
+// second way to acess property using "dot notation"
+if (gandalf.haveRetirementPlan) {
+  // or creating a variable that matches the name of property
+
+  var ageProperty = "age (est)";
+  var years = gandalf[ageProperty]; ///"gandalf property + "age (est)"
+  alert("my 401k has gathered interest for " + years + "years!");
+}
+
+// acessing array & porperty from an ibject
+alert(" i have more than " + gandalf.aliases.length + " aliases ");
+/////////////// displays number of items in string
+
+// non-existant properties returned modified
+alert("my designation is " + gandalf[" designation"]);
