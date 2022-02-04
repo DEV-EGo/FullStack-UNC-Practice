abab0f431172a47a0efb2d436742c3602fe90123
Edwin <dev.edwing@gmail.com>
2022-02-04 12:37:01 -0500

    fetch response

--- JS_Dinner_App/intel.js
+++ JS_Dinner_App/intel.js
@@ -4,9 +4,15 @@ const container = document.querySelector("container");
 let searchQuery = "";
 const APP_ID = "4b4a492a";
 const APP_Key = "5b9ed93a32ba8d37635a6c868d15c084";
-const baseURL = `https://api.edamam.com/api/recipes/v2=pizza&app_id=${APP_ID}&app_key=${APP_Key}`;
+const baseURL = `https://api.edamam.com/api/recipes/v2/search?=pizza&app_id=${APP_ID}&app_key=${APP_Key}`;
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   searchQuery = e.target.querySelector("input").value;
+  fetchAPI();
 });
+async function fetchAPI() {
+  const baseURL = `https://api.edamam.com/api/recipes/v2/search?=pizza&app_id=${APP_ID}&app_key=${APP_Key}`;
+  const response = await fetch(baseURL);
+  console.log(reposnse);
+}
