ccdcd5703877d518dbf3288dcd27998d2e6dfb32
Edwin <dev.edwing@gmail.com>
2022-02-04 12:33:30 -0500

    app id

--- JS_Dinner_App/intel.js
+++ JS_Dinner_App/intel.js
@@ -3,6 +3,8 @@ const searchResultDiv = document.querySelector(".search-result");
 const container = document.querySelector("container");
 let searchQuery = "";
 const APP_ID = "4b4a492a";
+const APP_Key = "5b9ed93a32ba8d37635a6c868d15c084";
+const baseURL = `https://api.edamam.com/api/recipes/v2=pizza&app_id=${APP_ID}&app_key=${APP_Key}`;
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
