eae379e33cbfed4d0e883833c9f69c3dd2804ba0
Edwin <dev.edwing@gmail.com>
2022-02-03 20:57:24 -0500

    app id

--- JS_Dinner_App/intel.js
+++ JS_Dinner_App/intel.js
@@ -1,3 +1,10 @@
 const searchForm = document.querySelector("form");
 const searchResultDiv = document.querySelector(".search-result");
 const container = document.querySelector("container");
+let searchQuery = "";
+const APP_ID = "4b4a492a";
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchQuery = e.target.querySelector("input").value;
+});
