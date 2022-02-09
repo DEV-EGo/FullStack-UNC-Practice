30ced00f9189a89f84bcd61a91e4dcd6c9c1bcc5
Edwin <dev.edwing@gmail.com>
2022-02-09 12:43:09 -0500

    appenchild technique

--- JQUERY/DOM-Manipulation/javascript.js
+++ JQUERY/DOM-Manipulation/javascript.js
@@ -1,1 +1,12 @@
-alert("this works");
+var targetID = document.getElementById("empty-div");
+// using "textcontent" method to change content inside the div
+targetID.textContent = "Hello Motto";
+// adding to the div and replacing what was already inside the div
+targetID.textContent = " top of the morning to you";
+
+// creating a new div "newDiv"
+var newDiv = document.createElement("div");
+// inserting text inside the new div
+newDiv.textContent = " this is a new div";
+//using the "appendchild" method to combine both of the divs on the page
+targetID.appendChild(newDiv);
