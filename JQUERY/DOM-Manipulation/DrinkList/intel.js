7ab7b2de19d211d39c7755dc7a5279230634a25a
Edwin <dev.edwing@gmail.com>
2022-02-15 12:39:19 -0500

    targeting query selectors

---
+++ JQUERY/DOM-Manipulation/DrinkList/intel.js
@@ -0,0 +1,30 @@
+// Array holds all of the drinks available
+var drinkList = [
+  "Coffee: $5",
+  "Espresso: $7",
+  "Cappuccino: $6",
+  "Latte: $4",
+  "Tea: $3",
+  "Ice Coffee: $6",
+  "Ice Espresso: $8",
+  "Ice Latte: $6",
+  "Ice Tea: $4",
+];
+
+// 1. Create code that "grabs" the div with the matching id (#drink-options);
+// ...
+
+var drinkList = document.getElementById("drink-options");
+// ...
+
+// 2. Create a for loop that creates HTML content of all the drinks using JavaScript.
+// HINT: You will need to use each of the following methods: createElement, textContent, appendChild
+// ...
+for (var i = 0; i < drinkList.length; i++) {
+  var pourUp = document.createElement("p");
+
+  pourUp.textContent = drinkList[i];
+
+  drinkList[i].appendChild(pourUp);
+}
+// ...
