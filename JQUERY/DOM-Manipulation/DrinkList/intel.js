ccc9b49a80c407685d985b57af07948b8dc49426
Edwin <dev.edwing@gmail.com>
2022-02-16 12:25:42 -0500

    game list

--- JQUERY/DOM-Manipulation/DrinkList/intel.js
+++ JQUERY/DOM-Manipulation/DrinkList/intel.js
@@ -1,30 +1,49 @@
 // Array holds all of the drinks available
-var drinkList = [
-  "Coffee: $5",
-  "Espresso: $7",
-  "Cappuccino: $6",
-  "Latte: $4",
-  "Tea: $3",
-  "Ice Coffee: $6",
-  "Ice Espresso: $8",
-  "Ice Latte: $6",
-  "Ice Tea: $4",
-];
+// var drinkList = [
+//   "Coffee: $5",
+//   "Espresso: $7",
+//   "Cappuccino: $6",
+//   "Latte: $4",
+//   "Tea: $3",
+//   "Ice Coffee: $6",
+//   "Ice Espresso: $8",
+//   "Ice Latte: $6",
+//   "Ice Tea: $4",
+// ];
 
 // 1. Create code that "grabs" the div with the matching id (#drink-options);
 // ...
 
-var drinkDiv = document.getElementById("drink-options");
+// var drinkDiv = document.getElementById("drink-options");
 // ...
 
 // 2. Create a for loop that creates HTML content of all the drinks using JavaScript.
 // HINT: You will need to use each of the following methods: createElement, textContent, appendChild
 // ...
-for (var i = 0; i < drinkList.length; i++) {
-  var pourUp = document.createElement("p");
+// for (var i = 0; i < drinkList.length; i++) {
+//   var pourUp = document.createElement("p");
 
-  pourUp.textContent = drinkList[i];
+//   pourUp.textContent = drinkList[i];
 
-  drinkDiv.appendChild(pourUp);
-}
+//   drinkDiv.appendChild(pourUp);
+// }
 // ...
+
+var gamelist = [
+  "Call of Duty",
+  "Gears of war",
+  "Need For Speed",
+  "Crash Bandicoot",
+  "Pokemon",
+  "Metroid",
+  "Spider-Man",
+];
+var DisplayingListOfGame = document.getelementById("game-list");
+
+for (var i = 0; i < gamelist.length; i++) {
+  var showGames = document.createElement("p");
+
+  showGames.textContent = gamelist[i];
+
+  DisplayingListOfGame.appendChild(showGames);
+}
