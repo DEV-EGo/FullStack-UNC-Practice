d814ef73d1637ff392a151fc7ee10ad2a0c7f509
Edwin <dev.edwing@gmail.com>
2022-02-28 12:55:34 -0500

    javascript start up

--- timers/wordguess/logic.js
+++ timers/wordguess/logic.js
@@ -1,1 +1,24 @@
-alert("hey");
+// array of words to choose from
+var wordlist = ["Eppi", "Jordan", "Gracie", "Bryan", "Ricky", "katherine"];
+
+// Computer selected solutions held here
+var chosenWord = "";
+
+// this will break the solution into individual letters to be stored
+var lettersInChosenWord = [];
+
+// This will be the number of blanks we show we based on the solutions
+var blanksAndSuccess = [];
+
+// Holding the wrong solutions
+var wrongGuesses = [];
+
+// Holds all the letters guessed
+var letterGuessed = [];
+
+// Game counter
+var winsCounter = 0;
+var lossCounter = 0;
+var numGuesses = 9;
+
+// FUNCTIONS
