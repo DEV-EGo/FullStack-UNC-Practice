9d5c1514dbd213a55fb9a0a5aad573dbf85e2ec9
Edwin <dev.edwing@gmail.com>
2022-03-07 12:53:11 -0500

    timer alert

---
+++ timers/this/intel.js
@@ -0,0 +1,21 @@
+// Variable that will hold the button alert timeout when it is closed
+var delayButtonAlert;
+
+// time out in javascript
+// set the window alert to run 1 second after the time out  when its clicked
+var windowTimeout = setTimeout(function () {
+  // setting time out alert to run 3 seconds after its called
+  delayButtonAlert = setTimeout(function () {
+    alert("alert #2: called 3 seconds after the start button is clicked");
+  });
+});
+
+// cancel on click on
+$("#cancel").on("click", function () {
+  clearTimeout(delayButtonAlert);
+});
+
+// cancel window alert on clicked
+$("#window-cancel").on("click", function () {
+  clearTimeout(windowTimeout);
+});
