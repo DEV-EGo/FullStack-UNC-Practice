6b0e0b56e0749c8547e819eacebb4d4cc1921512
Edwin <dev.edwing@gmail.com>
2022-03-14 12:47:23 -0400

    timer practice, file set up

---
+++ timers/Simple_Timer/intel.js
@@ -0,0 +1,11 @@
+// simple timer practice
+
+// step:1
+// using Audio file saved in folder
+var audio = new Audio("raven.mp3");
+audio.load();
+
+// step:2
+//  after 5 seconds, execute the fiveSeconds function
+//  after 10 seconds, execute the tenSeconds function
+//  after 15 seconds, execute the timeUp function
