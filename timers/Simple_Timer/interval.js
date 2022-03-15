e96823d8fca2ea53821b981b3193ab88aa05169d
Edwin <dev.edwing@gmail.com>
2022-03-15 12:38:51 -0400

    interval practice

--- timers/Simple_Timer/interval.js
+++ timers/Simple_Timer/interval.js
@@ -13,3 +13,39 @@ $("#stop").on("click", stop);
 
 // when the resumer button gets clicked , execute the function
 $("#resume").on("click", run);
+
+// the run functino set interval
+// that runs the decrement function once a seconds
+function run() {
+  clearingInterval(intervalId);
+  intervalId = setInterval(decrement, 1000);
+}
+
+// ==== decrement function
+function decrement() {
+  // decrease number
+  number--;
+
+  //   show number inside the #show-number div
+
+  $("#show-number").html("<h2>" + number + "</h2>");
+
+  //   once number hits 0
+
+  if (number === 0) {
+    // run stop function
+    stop();
+
+    // alert user the time is up
+    alert("time is up");
+  }
+}
+
+function stop() {
+  // clear our intervalId
+  // pass the name of the interval , clear the function
+
+  clearingInterval(intervalId);
+}
+
+run();
