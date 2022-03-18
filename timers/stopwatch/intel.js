d4871004a79e86c78b0f513a5aadc4ffbec6addd
Edwin <dev.edwing@gmail.com>
2022-03-18 12:51:00 -0400

    functino

--- timers/stopwatch/intel.js
+++ timers/stopwatch/intel.js
@@ -17,25 +17,50 @@ var lap = 1;
 function reset() {
   time = 0;
   lap = 1;
+
+  $("#display").text("");
+  $("#laps").text("");
 }
 
 function start() {
   // set interval to start the count here and set the clock clockRunning
   if (!clockRunning) {
+    intervalId = setInterval(count, 1000);
+    clockRunning = true;
   }
 }
 
 function stop() {
   // stops the count and set the clock to not be running
+
+  //   clearinterval stops the count and sets the clock to not runnning
+  clearInterval(intervalId);
+  clockRunning = false;
 }
 
 function recordLap() {
   // gets current time and passes that into thte timecoverter function
   // save the variables
+  var converted = timeConverter(time);
+
+  $("#laps").append("<p> lap: " + lap + ":" + converted + "</p>");
+
+  //done: incremet lap by 1. remember we can use "this" here instead
+
+  lap++;
 }
 
 function count() {
   // increment time by 1, remember we cant use "this" here
+
+  time++;
+
+  //  get the current time and pass into the timeconverter function
+  var converted = timeconverter(time);
+  console.log(converted);
+
+  //   using variable we just created to show the converted time in the display
+  $("#display").text(converted);
 }
 
 function timeCoverter(t) {
