a1a79d2b8a334513967dd67539d88462136952c1
Edwin <dev.edwing@gmail.com>
2022-03-30 12:59:32 -0400

    add

--- AJAX/OMBD/intel.js
+++ AJAX/OMBD/intel.js
@@ -1,1 +1,4 @@
 // constructing a queryURL variable we will use instead of the literal string inside of the ajax method
+var title = "space+jam";
+var queryURL =
+  "https://www.ombdapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
