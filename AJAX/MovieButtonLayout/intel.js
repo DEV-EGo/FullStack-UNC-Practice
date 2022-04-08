3274636590083a57803cbd9abb8bad1b7837f8b7
Edwin <dev.edwing@gmail.com>
2022-04-08 12:26:24 -0400

    html changes

--- AJAX/MovieButtonLayout/intel.js
+++ AJAX/MovieButtonLayout/intel.js
@@ -1,39 +1,54 @@
-var movies = ["The matrix", "The notebook", "Mr.Nobody", "The lion king"];
-// function displaying movie data
+// Initial array of movies
+var movies = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
+
+// Generic function for capturing the movie name from the data-attribute
+function alertMovieName() {
+  var movieName = $(this).attr("data-name");
+
+  alert(movieName);
+}
+
+// Function for displaying movie data
 function renderButtons() {
-  // deleting the movie button prior to adding a new movie
+  // Deleting the movies prior to adding new movies
+  // (this is necessary otherwise we will have repeat buttons)
+  $("#buttons-view").empty();
 
-  $("#movies-view").empty();
-
-  //   looping through the array of movies
-
-  for (var i = 0; i < movies.lenght; i++) {
-    // generating buttons for each movie
-    // this code $("<button></button>") is all jquery  needs to create starts and end tag
-
+  // Looping through the array of movies
+  for (var i = 0; i < movies.length; i++) {
+    // Then dynamicaly generating buttons for each movie in the array
+    // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
     var a = $("<button>");
-
-    // adding a class
+    // Adding a class of movie to our button
     a.addClass("movie");
-
-    // adding data attribute with  a value of the movie
-    a.attr("data-name"), movies[i];
-
-    // providing the buttons text with a value of the movie
+    // Adding a data-attribute
+    a.attr("data-name", movies[i]);
+    // Providing the initial button text
     a.text(movies[i]);
-
-    // adding button the html
-    $("#movies-view").append(a);
+    // Adding the button to the HTML
+    $("#buttons-view").append(a);
   }
 }
 
+// This function handles events where one button is clicked
 $("#add-movie").on("click", function (event) {
+  // Preventing the buttons default behavior when clicked (which is submitting a form)
   event.preventDefault();
-
+  // This line grabs the input from the textbox
   var movie = $("#movie-input").val().trim();
 
+  // Adding the movie from the textbox to our array
   movies.push(movie);
 
+  // Calling renderButtons which handles the processing of our movie array
   renderButtons();
 });
+
+// Function for displaying the movie info
+// We're adding a click event listener to all elements with the class "movie"
+// We're adding the event listener to the document because it will work for dynamically generated elements
+// $(".movies").on("click") will only add listeners to elements that are on the page at that time
+$(document).on("click", ".movie", alertMovieName);
+
+// Calling the renderButtons function to display the intial buttons
 renderButtons();
